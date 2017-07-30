df6c1116ffaa515a5185dc93d0eba1fb92b2b8de
Mookrs <mookrs@gmail.com>
2017-07-31 00:46:54 +0800

    Add IsItFridayBot

---
+++ fanfou_bot/isitfriday/isitfriday.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import datetime
+import json
+import os
+import random
+
+from ..basebot import BaseBot, get_abs_path
+
+
+class IsItFridayBot(BaseBot):
+    today = datetime.datetime.now().isoweekday()
+
+    def __init__(self, *args, **kwargs):
+        super(IsItFridayBot, self).__init__(*args, **kwargs)
+
+    def get_status(self):
+        with open(get_abs_path(__file__, 'statuses.json')) as statuses_file:
+            statuses = json.load(statuses_file)
+        return random.choice(statuses[str(self.today)])
+
+    def get_img_path(self, top):
+        images = []
+        for root, dirs, files in os.walk(top):
+            for file in files:
+                images.append(os.path.join(root, file))
+        return random.choice(images)
+
+    def get_random_img(self, dir_name):
+        image = self.get_img_path(get_abs_path(__file__, dir_name))
+        print(image)
+        with open(image, 'rb') as imagefile:
+            return imagefile.read()
+
+    def run(self, is_special=False):
+        status = self.get_status()
+
+        if is_special:
+            imagedata = self.get_random_img('special')
+        elif self.today == 5:
+            imagedata = self.get_random_img('true')
+        else:
+            imagedata = self.get_random_img('false')
+
+        self.update_status(status=status, photo=imagedata)
