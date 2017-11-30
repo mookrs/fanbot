d89ef411686a81194cc95e70315a4dcac6dfcbfc
Mookrs <mookrs@gmail.com>
2017-11-30 20:15:17 +0800

    Clean moegirl code

--- fanfou_bot/moegirl_daily/moegirl_daily.py
+++ fanfou_bot/moegirl_daily/moegirl_daily.py
@@ -5,54 +5,52 @@ from urllib.request import quote
 
 from ..spiderbot import SpiderBot
 
-API_RANDOM_PAGE = 'https://zh.moegirl.org/api.php?format=json&action=query&list=random&rnnamespace=0&rnlimit=1&continue='
-
 
 class MoegirlDailyBot(SpiderBot):
     def __init__(self, *args, **kwargs):
         super(MoegirlDailyBot, self).__init__(*args, **kwargs)
-        self.opener = self.make_opener(('User-agent', 'Mozilla/5.0'))
+        self.opener = self.make_opener(('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3269.3 Safari/537.36'))
 
     def fetch_page(self):
-        response = self.open_url(API_RANDOM_PAGE)
+        # See https://zh.moegirl.org/api.php?action=help&modules=query%2Brandom
+        # rnnamespace: 只返回这些名字空间的页面
+        api_random_page = 'https://zh.moegirl.org/api.php?action=query&list=random&rnnamespace=0&format=json'
+        response = self.open_url(api_random_page)
         data = json.loads(response.read().decode('utf-8'))
+
         page_id = data['query']['random'][0]['id']
         page_title = data['query']['random'][0]['title']
         return page_id, page_title
 
     def fetch_summary(self, page_url):
-        # Use lxml instead of default html parser will get better soup
         soup = self.make_soup(page_url, self.opener, parser='lxml')
         summary = ''
 
-        # 处理「战舰少女」专题
         if page_url.startswith('https://zh.moegirl.org/%E6%88%98%E8%88%B0%E5%B0%91%E5%A5%B3:'):
-            summary = soup.find('div', {'class': 'infotemplatebox'}).find_next_sibling('p').get_text(strip=True)
-        # 消歧页去掉摘要
-        elif soup.find(['div', 'table'], {'id': 'disambigbox'}):
+            # `战舰少女` 专题
+            summary = soup.find('div', 'infotemplatebox').find_next_sibling('p').get_text(strip=True)
+        elif soup.find(['div', 'table'], id='disambigbox'):
+            # 消歧页去掉摘要
             pass
-        # 正常情况
         else:
-            contents = soup.find('div', {'id': 'mw-content-text'}).find_all('p', recursive=False)
-            for content in contents:
-                summary = content.get_text(strip=True)
-                if summary:
-                    if summary.startswith('.bilibili-video-button'):
-                        summary = ''
+            paragraphs = soup.find(id='mw-content-text').find_all('p', recursive=False)
+            for paragraph in paragraphs:
+                summary = paragraph.get_text(strip=True)
+                if summary and not summary.startswith('window.RLQ'):
                     break
 
         return summary
 
     def fetch_image(self, page_id):
-        api_page_image = 'https://zh.moegirl.org/api.php?format=json&action=query&pageids={}&prop=pageimages&pithumbsize=500'.format(page_id)
-        response = self.open_url(api_page_image, self.opener)
+        api_page_image = 'https://zh.moegirl.org/api.php?action=query&pageids={}&prop=pageimages&pithumbsize=500&format=json'.format(page_id)
+        response = self.open_url(api_page_image)
         data = json.loads(response.read().decode('utf-8'))
 
         page_infos = data['query']['pages'][str(page_id)]
         thumbnail = page_infos.get('thumbnail')
         if thumbnail is not None:
-            thumbnail_source = page_infos['thumbnail']['source']
-            thumbnail_response = self.open_url(thumbnail_source)
+            thumbnail_source = thumbnail['source']
+            thumbnail_response = self.open_url(thumbnail_source, self.opener)
             return thumbnail_response.read() if thumbnail_response is not None else None
 
         return None
@@ -61,15 +59,9 @@ class MoegirlDailyBot(SpiderBot):
         page_id, page_title = self.fetch_page()
 
         page_url = 'https://zh.moegirl.org/{}'.format(quote(page_title, safe='/:'))
-
-        # Avoid HTTP Error 429: Too Many Requests
-        time.sleep(30)
         page_summary = self.fetch_summary(page_url)
-        if len(page_url) >= 40:
-            page_url = self.shorten_url(page_url)
 
         status = '【{}】{} {}'.format(page_title, page_url, page_summary)
 
-        time.sleep(60)
         page_image = self.fetch_image(page_id)
         self.update_status(status, photo=page_image)
