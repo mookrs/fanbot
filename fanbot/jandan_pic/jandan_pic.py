4e7af2b9829ebcdca9f374d5e0ec7a7334680715
Mookrs <mookrs@gmail.com>
2018-02-23 16:44:52 +0800

    Decrypt jandan pic to make it work again

--- fanbot/jandan_pic/jandan_pic.py
+++ fanbot/jandan_pic/jandan_pic.py
@@ -2,6 +2,11 @@
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
+import base64
+import re
+
+from hashlib import md5
+
 from ..basebot import get_abs_path
 from ..spiderbot import SpiderBot
 from ..db import DBHelper
@@ -9,29 +14,91 @@ from ..db import DBHelper
 DATABASE = get_abs_path(__file__, 'jandan_pic.db')
 db = DBHelper(DATABASE)
 PIC_URL = 'https://jandan.net/pic'
+PATTERN_X = re.compile('f\.remove\(\);var c=.+?\(e,"(.+?)"\)')
 
 
 class JandanPicBot(SpiderBot):
     def __init__(self, *args, **kwargs):
         super(JandanPicBot, self).__init__(*args, **kwargs)
-        self.opener = self.make_opener(('User-agent', 'Mozilla/5.0'), ('Cookie', '458528247=db856X2bSPJdJD3mZ0qNgqHxstlcw%2BC4xtmr%2BPfjKA; jdna=596e6fb28c1bb47f949e65e1ae03f7f5#1466510995815'))
+        self.opener = self.make_opener(
+            ('User-agent', 'Mozilla/5.0'),
+            ('Cookie', '__cfduid=d669a4b2c49aa6fc3759bab643098b8b21503417105')
+        )
+        self.x = ''
+
+    def get_x(self, js_url):
+        js = self.open_url(js_url, self.opener)
+        x = PATTERN_X.search(js.read().decode('utf-8')).group(1)
+        return x
 
     def get_last_page(self):
-        soup = self.make_soup(PIC_URL)
+        soup = self.make_soup(PIC_URL, self.opener)
+
+        js_url = soup.find('script', {'src': lambda x: x and '//cdn.jandan.net/static/min/' in x})['src']
+        if not js_url.startswith('http:'):
+            js_url = 'http:' + js_url
+        self.x = self.get_x(js_url)
+
         span = soup.find('span', {'class': 'current-comment-page'})
         return int(span.get_text()[1:-1])
 
     def low_score(self, item):
         ooxx = item.find('div', {'class': 'jandan-vote'}, recursive=False).find_all('span')
-        oo = int(ooxx[1].get_text())
-        xx = int(ooxx[3].get_text())
+        oo = int(ooxx[2].get_text())
+        xx = int(ooxx[4].get_text())
         if oo < 15 or xx * 3 > oo:
             return True
         return False
 
+    def decrypt(self, n):
+        g = 4
+        x = md5(self.x.encode('utf-8')).hexdigest()
+        w = md5(x[:16].encode('utf-8')).hexdigest()
+        u = md5(x[16:].encode('utf-8')).hexdigest()
+
+        t = n[:g]
+        r = w + md5((w + t).encode('utf-8')).hexdigest()
+
+        n = n[g:]
+        m = base64.b64decode(n + (4 - len(n) % 4) * '=')
+
+        h = list(range(256))
+        q = [ord(r[i % 64]) for i in range(256)]
+        o = 0
+        for p in range(256):
+            o = (o + h[p] + q[p]) & 0xFF
+            h[p], h[o] = h[o], h[p]
+
+        l = ''
+        v = 0
+        o = 0
+        for p in m:
+            v = (v + 1) & 0xFF
+            o = (o + h[v]) & 0xFF
+            h[v], h[o] = h[o], h[v]
+            l += chr(int(p) ^ (h[(h[v] + h[o]) & 0xFF]))
+        l = l[26:]
+        if not l.startswith('http:'):
+            l = 'http:' + l
+        return l
+
+    def get_img_urls(self, img_hash_tags):
+        img_urls = []
+        for img_hash_tag in img_hash_tags:
+            img_hash = img_hash_tag.extract().get_text()
+            img_url = self.decrypt(img_hash)
+
+            img_url_parts = img_url.split('/')
+            img_url_parts[3] = 'large'
+            img_url = '/'.join(img_url_parts)
+
+            img_urls.append(img_url)
+
+        return img_urls
+
     def process_page(self, page):
         page_url = '{}/page-{}'.format(PIC_URL, page)
-        soup = self.make_soup(page_url, parser='lxml')
+        soup = self.make_soup(page_url, self.opener, parser='lxml')
 
         items = soup.find_all('div', {'class': 'row'})
         for item in reversed(items):
@@ -42,17 +109,14 @@ class JandanPicBot(SpiderBot):
             item_id = item_content.span.a.get_text()
             record = db.query('SELECT * FROM pictrue WHERE id=?', (item_id,))
             if not record:
-                text = item_content.p.get_text(strip=True).replace('[查看原图]', '')
-                img_tags = item.find_all('a', {'class': 'view_img_link'}) or item.find_all('img')
-                img_count = len(img_tags)
+                img_hash_tags = item_content.find_all('span', {'class': 'img-hash'})
+                img_urls = self.get_img_urls(img_hash_tags)
+                img_count = len(img_urls)
+                text = item_content.p.get_text(strip=True)
 
-                for index, img_tag in enumerate(img_tags, start=1):
+                for index, img_url in enumerate(img_urls, start=1):
                     prefix = '' if img_count == 1 else '({}/{}) '.format(index, img_count)
                     status = prefix + text if index == 1 else prefix
-
-                    img_url = img_tag.get('href') or img_tag.get('src')
-                    if img_url.startswith('//'):
-                        img_url = 'http:' + img_url
 
                     response = self.open_url(img_url, self.opener)
                     if not self.accepted_img_size(response):
