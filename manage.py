147c6e5d1a93d2e21d5bab05ae2c9df3ee1ff136
Mookrs <mookrs@gmail.com>
2018-08-26 23:08:38 +0800

    Improve import method in manage file

--- manage.py
+++ manage.py
@@ -1,22 +1,7 @@
 import argparse
 
-from fanbot.analects_chs import analects_chs
-from fanbot.analects_cht import analects_cht
-from fanbot.dictionary import dictionary
-from fanbot.fish import fish
-from fanbot.isitfriday import isitfriday
-from fanbot.jandan import jandan
-from fanbot.jandan_pic import jandan_pic
-from fanbot.moegirl_daily import moegirl_daily
-from fanbot.photo_of_the_day import photo_of_the_day
-from fanbot.reddit import reddit
-from fanbot.shuowen import shuowen
-from fanbot.taoteching import taoteching
-from fanbot.word_of_the_day import word_of_the_day
-from fanbot.yupian import yupian
 
-
-def main():
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--analects_chs', help='analects_chs bot', action='store_true')
     parser.add_argument('--analects_cht', help='analects_cht bot', action='store_true')
@@ -32,49 +17,71 @@ def main():
     parser.add_argument('--taoteching', help='taoteching bot', action='store_true')
     parser.add_argument('--word_of_the_day', help='word_of_the_day bot', action='store_true')
     parser.add_argument('--yupian', help='yupian bot', action='store_true')
+    return parser.parse_args()
 
-    args = parser.parse_args()
+
+def main():
+    """Main program.
+
+    Parse arguments, run bot.
+    """
+    args = parse_args()
+
     if args.analects_chs:
+        from fanbot.analects_chs import analects_chs
         bot = analects_chs.AnalectsChsBot()
         bot.run()
     elif args.analects_cht:
+        from fanbot.analects_cht import analects_cht
         bot = analects_cht.AnalectsChtBot()
         bot.run()
     elif args.dictionary:
+        from fanbot.dictionary import dictionary
         bot = dictionary.DictionaryBot()
         bot.run()
     elif args.fish:
+        from fanbot.fish import fish
         bot = fish.FishBot()
         # Here is start() for long run
         bot.start()
     elif args.isitfriday:
+        from fanbot.isitfriday import isitfriday
         bot = isitfriday.IsItFridayBot()
         bot.run()
     elif args.jandan:
+        from fanbot.jandan import jandan
         bot = jandan.JandanBot()
         bot.run()
     elif args.jandan_pic:
+        from fanbot.jandan_pic import jandan_pic
         bot = jandan_pic.JandanPicBot()
         bot.run()
     elif args.moegirl_daily:
+        from fanbot.moegirl_daily import moegirl_daily
         bot = moegirl_daily.MoegirlDailyBot()
         bot.run()
     elif args.photo_of_the_day:
+        from fanbot.photo_of_the_day import photo_of_the_day
         bot = photo_of_the_day.PhotoOfTheDayBot()
         bot.run()
     elif args.reddit:
+        from fanbot.reddit import reddit
         bot = reddit.RedditBot()
         bot.run()
     elif args.shuowen:
+        from fanbot.shuowen import shuowen
         bot = shuowen.ShuowenBot()
         bot.run()
     elif args.taoteching:
+        from fanbot.taoteching import taoteching
         bot = taoteching.TaoTeChingBot()
         bot.run()
     elif args.word_of_the_day:
+        from fanbot.word_of_the_day import word_of_the_day
         bot = word_of_the_day.WordOfTheDayBot()
         bot.run()
     elif args.yupian:
+        from fanbot.yupian import yupian
         bot = yupian.YupianBot()
         bot.run()
 
