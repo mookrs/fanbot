0161d25fd3a68b1c3654699a84b48393de3ba6fa
Mookrs <mookrs@gmail.com>
2018-11-07 15:48:49 +0800

    Make it can use more flexible profile json file

--- fanbot/isitfriday/isitfriday.py
+++ fanbot/isitfriday/isitfriday.py
@@ -8,36 +8,50 @@ from ..basebot import BaseBot, get_abs_path
 
 
 class IsItFridayBot(BaseBot):
-    today = datetime.datetime.now().isoweekday()
+    now = datetime.datetime.now()
+    weekday = str(now.isoweekday())
+    date = now.strftime('%Y%m%d')
 
     def __init__(self, *args, **kwargs):
         super(IsItFridayBot, self).__init__(*args, **kwargs)
 
-    def get_status(self):
-        with open(get_abs_path(__file__, 'statuses.json')) as statuses_file:
-            statuses = json.load(statuses_file)
-        return random.choice(statuses[str(self.today)])
+    def load_profile(self):
+        with open(get_abs_path(__file__, 'profile.json')) as f:
+            profile = json.load(f)
+        return profile
 
-    def get_img_path(self, top):
+    def read_image(self, image_path):
+        abs_image_path = get_abs_path(__file__, image_path)
+        with open(abs_image_path, 'rb') as f:
+            return f.read()
+
+    def get_random_image(self, top):
+        abs_top = get_abs_path(__file__, top)
         images = []
-        for root, dirs, files in os.walk(top):
+        for root, _, files in os.walk(abs_top):
             for file in files:
                 images.append(os.path.join(root, file))
         return random.choice(images)
 
-    def get_random_img(self, dir_name):
-        image = self.get_img_path(get_abs_path(__file__, dir_name))
-        with open(image, 'rb') as imagefile:
-            return imagefile.read()
-
-    def run(self, special_image=False, status=None):
-        status = self.get_status() if status is None else status
-
-        if special_image:
-            imagedata = self.get_random_img('special')
-        elif self.today == 5:
-            imagedata = self.get_random_img('true')
+    def get_image_data(self, info):
+        image_type = info.get('image_type')
+        if image_type == 'false':
+            image_path = self.get_random_image('false')
+        elif image_type == 'true':
+            image_path = self.get_random_image('true')
+        elif image_type == 'custom':
+            image_path = info['image_path']
         else:
-            imagedata = self.get_random_img('false')
+            return None
 
-        self.update_status(status=status, photo=imagedata)
+        image_data = self.read_image(image_path)
+        return image_data
+
+    def run(self):
+        profile = self.load_profile()
+        info = profile[self.date] if self.date in profile else profile[self.weekday]
+
+        status = random.choice(info['statuses'])
+        image_data = self.get_image_data(info)
+
+        self.update_status(status=status, photo=image_data)
