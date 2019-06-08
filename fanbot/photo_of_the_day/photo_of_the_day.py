df1030037cea08a68393e9e9cb79614cd4105b5d
mookrs <mookrs@gmail.com>
2019-06-08 22:41:33 +0800

    Update photo of the day

--- fanbot/photo_of_the_day/photo_of_the_day.py
+++ fanbot/photo_of_the_day/photo_of_the_day.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python3
 import json
-# import time
-
-# from selenium import webdriver
 
 from ..basebot import get_abs_path
 from ..spiderbot import SpiderBot
@@ -20,62 +16,32 @@ class Bot(SpiderBot):
         super(Bot, self).__init__(*args, **kwargs)
         self.opener = self.make_opener(
             ('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3642.0 Safari/537.36'),
-            ('Cookie', 'cf_clearance=659f463d4cd13ba504a4ff23e5e50aefc1bd01a0-1545889065-57600-150; __cfduid=d4485a961d407f544006a125f0835a2541545889059')
+            ('Cookie', '__cfduid=d106044aba80f86e5d8d1270c45ee78411559650908')
         )
 
-    # def init_driver(self):
-    #     options = webdriver.ChromeOptions()
-    #     options.add_argument('--headless')
-    #     options.add_argument('--disable-gpu')
-    #     return webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=options)
+    def replace_redundant_words(self, old_status):
+        new_status = old_status.replace('你来掌镜摄影师', '摄影师').replace('由你掌镜摄影师', '摄影师').replace('Your Shot摄影师', '摄影师')
+        return new_status
 
     def page_exist(self, page_url):
         record = db.query('SELECT * FROM pictrue WHERE page_url=?', (page_url,))
         return True if record else False
 
-    def get_recent_page_info(self):
-        # self.driver.get(API_IMG_LIST)
-        # time.sleep(10)
-
-        # cookies = self.driver.get_cookies()
-        # cookie_list = [cookie['name'] + '=' + cookie['value'] for cookie in cookies if cookie['domain'] == '.ngchina.com.cn']
-        # cookie_str = '; '.join(cookie_list)
-
-        # data = json.loads(self.driver.find_element_by_tag_name('body').text)
-
-        response = self.open_url(API_IMG_LIST, self.opener)
-        data = json.loads(response.read().decode('utf-8'))
-
-        relative_url = data[0]['url']
-        title = data[0]['title'][5:].strip()
-        desc = data[0]['description']
-
-        return BASE_URL + relative_url, title, desc
-
     def get_long_desc(self, article_con):
-        if (article_con.div and article_con.div.get_text().strip() and
-                not article_con.div.get_text().strip().startswith('摄影：') and
-                (not article_con.div.has_attr('class') or
-                    (article_con.div.has_attr('class') and
-                     article_con.div['class'][0] != 'counsel'))):
-            # 想获取的页面形如
-            # http://www.ngchina.com.cn/photography/photo_of_the_day/5281.html
-            # 需要排除的页面形如
-            # <div>&nbsp;</div>
-            # http://www.ngchina.com.cn/photography/photo_of_the_day/5288.html
-            # <div class="counsel">
-            # http://www.ngchina.com.cn/photography/photo_of_the_day/5127.html
-            long_desc = article_con.div.get_text().strip()
-        else:
-            long_desc = article_con.get_text().strip().split('\n')[0]
-
+        # <div ...>[desc]</div> + <div>&nbsp;</div> + ...
+        # http://www.ngchina.com.cn/photography/photo_of_the_day/5281.html
+        # <div ...>[desc] <br> <br> 摄影：xxxx，你来掌镜YOURSHOT</div> + ...
+        # http://www.ngchina.com.cn//photography/photo_of_the_day/5913.html#
+        # [desc] + <div>&nbsp;</div> + ...
+        # http://www.ngchina.com.cn/photography/photo_of_the_day/5288.html
+        # [desc] <br> <br> 摄影：xxxx，你来掌镜YOURSHOT + ...
+        # http://www.ngchina.com.cn//photography/photo_of_the_day/5908.html
+        # <span ...>[desc]</span> + ...
+        # http://www.ngchina.com.cn/photography/photo_of_the_day/5127.html
+        long_desc = article_con.get_text().strip().split('\n')[0]
         return long_desc
 
     def get_page_detail(self, page_url):
-        # self.driver.get(page_url)
-        # html = self.driver.page_source
-        # soup = self.make_soup_with_html(html, 'lxml')
-
         soup = self.make_soup(page_url, self.opener)
 
         img_url = soup.find('div', {'class': 'tab_imgs'}).find('img').get('src')
@@ -85,9 +51,15 @@ class Bot(SpiderBot):
 
         return img_url, long_desc
 
-    def replace_redundant_words(self, old_status):
-        new_status = old_status.replace('你来掌镜摄影师', '摄影师').replace('由你掌镜摄影师', '摄影师')
-        return new_status
+    def get_recent_page_info(self):
+        response = self.open_url(API_IMG_LIST, self.opener)
+        data = json.loads(response.read().decode('utf-8'))
+
+        relative_url = data[0]['url']
+        title = data[0]['title'][5:].strip()
+        desc = data[0]['description']
+
+        return BASE_URL + relative_url, title, desc
 
     def run(self):
         db.execute('CREATE TABLE IF NOT EXISTS pictrue (`page_url`, `img_url`)')
@@ -105,7 +77,6 @@ class Bot(SpiderBot):
 
         response = self.open_url(img_url, self.opener)
         result = self.update_status(status, photo=response.read(), timeout=30)
-        # Saves to database
         if result:
             db.execute('INSERT INTO pictrue (`page_url`, `img_url`) VALUES (?,?)', (page_url, img_url))
             db.commit()
