c51510951477ed6bf14878bd16972cbfd1afe43f
mookrs <mookrs@gmail.com>
2019-06-09 13:02:21 +0800

    Change type meaning

--- fanbot/dictionary/dictionary.py
+++ fanbot/dictionary/dictionary.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
 from ..basebot import BaseBot, get_abs_path
 from ..db import Database
 
 DATABASE = get_abs_path(__file__, 'dictionary.db')
 db = Database(DATABASE)
+MAX_ID = 12394
 
 
 class Bot(BaseBot):
@@ -12,18 +12,20 @@ class Bot(BaseBot):
 
     def make_status(self):
         row_id = self._get_current_index()
-        self._set_next_index(row_id)
+        self._set_next_index(row_id, MAX_ID)
 
         code = 'CP000000000' + str(10000000 + row_id)
-        row = db.query("SELECT * FROM xinhua WHERE code='{}';".format(code), one=True)
-        item, explanation, item_type = row['item'], row['explanation'], row['type']
+        row = db.query(f"SELECT * FROM xinhua WHERE code='{code}';", one=True)
+        item, definition, item_type = row['item'], row['definition'], row['type']
 
         # type 表示的含义:
-        # 1 单字 2 词语
-        # 3 Unicode 扩展区的字
-        # 4 生造字或词语，未来可能出现在 Unicode 中
-        # 5 重复项或页面不存在
-        status = '{} {}'.format(item, explanation) if item_type in (1, 3) else None
+        # 1 单字
+        # 2 词语
+        # 3 Unicode 扩展区的单字
+        # 4 使用 Ideographic Description Sequences (IDS) 描述的单字
+        # 5 使用 Ideographic Description Sequences (IDS) 描述的词语
+        # 6 重复项或页面不存在
+        status = f'{item} {definition}' if item_type in (1, 3, 4) else None
         return status
 
     def run(self):
